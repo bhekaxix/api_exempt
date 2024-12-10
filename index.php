6921d9beffb483cb3d6902ab2f9ea89278025e6f
Taonga Bheka <taongabp@gmail.com>
2024-12-10 09:30:17 +0300

    second vommand

--- index.php
+++ index.php
@@ -1,12 +1,13 @@
 <?php
 
 // Importing content from an external file
-require_once "classes/layout.php";
-require_once "contents/contents.php";
+//require_once "classes/layout.php";
+//require_once "contents/contents.php";
+require_once "Autoload.php";
 
 // Creating an instance of a class
-$ObjLayout = new layout();
-$ObjContent = new contents();
+//$ObjLayout = new layout();
+//$ObjContent = new contents();
 
 // Invoke a method
 $ObjLayout->heading();
